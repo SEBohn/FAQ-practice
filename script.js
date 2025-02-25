135048d8d339b18747ad261ba84e46b1574f958b
Jinnis <bohnsarahe@gmail.com>
2025-02-25 09:50:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//Toggle questions
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
